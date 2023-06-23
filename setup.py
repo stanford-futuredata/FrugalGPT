09852b81123e46da1ba7a8a7f6ed8e10132ca72c
Lingjiao Chen <lingjiao@GFQ4L7GX7T.local>
2023-06-22 22:10:03 -0700

    setup files

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FrugalGPT',
+    version='0.0.1',
+    author='Lingjiao Chen, Matei Zaharia, and James Zou',
+    author_email='lingjiao@stanford.edu',
+    description='The FrugalGPT library',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'cohere',
+        'smart-open',
+        'jsonlines',
+        'anthropic',
+        'scikit-learn',
+        'evaluate',
+        'scipy',
+        'pandas',
+        'sqlitedict',
+        'torch',
+        'transformers',
+        'accelerate',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
